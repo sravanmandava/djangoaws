387835d67dca7e3501994d6449bbf186d1ed0305
sravanmandava <sravanmandava8@gmail.com>
2021-07-27 20:07:21 +0530

    Added static files

--- welcome/views.py
+++ welcome/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request,name):
-    return HttpResponse("<b> Hello "+ name +"</b>")
+    return render(request,"hello.html",{"name":name})
+
+    #return HttpResponse("<b> Hello "+ name +"</b>")
